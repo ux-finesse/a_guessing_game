3b147a511fc08f7a25c65e56a6262ac2991aafb1
Peter <olasunkanmifinesse@gmail.com>
2025-01-29 09:30:26 +0100

    made the copyright year dynamic

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-document.addEventListener('DOMContentLoaded', function (){
-    const randomNumber = Math.floor(Math.random() * 100) +1;
-    let attempts = 3;
+document.getElementById("year").textContent = new Date().getFullYear();
 
-    const valueInput = document.getElementById('valueInput');
-    const valueButton = document.getElementById('valueButton');
-    const feedback = document.getElementById('feedback');
+document.addEventListener("DOMContentLoaded", function () {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  let attempts = 3;
 
-    function handleGuess(){
-        const userGuess = parseInt(valueInput.value);
+  const valueInput = document.getElementById("valueInput");
+  const valueButton = document.getElementById("valueButton");
+  const feedback = document.getElementById("feedback");
 
-        // To check if input is not a number....
-        if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
-            feedback.textContent = '😁 Kindly enter a valid number between 1 and 100.';
-            feedback.style.color = 'red';
-            return;
-        }
+  function handleGuess() {
+    const userGuess = parseInt(valueInput.value);
 
-        attempts--;
-
-        if (userGuess === randomNumber) {
-            feedback.textContent = `🎉 Congratulations! You guessed the right number (${randomNumber}).`;
-            feedback.style.color = 'green';
-            valueButton.disabled = true;
-        } else if (attempts > 0) {
-            feedback.textContent = userGuess < randomNumber ? '🙄 Too low!,' : '🤨 Too high!,';
-            feedback.style.color = 'purple';
-            feedback.textContent += ` You have ${attempts} ${attempts === 1 ? 'attempt' : 'attempts'} left.`;
-        } else {
-            feedback.textContent = `😔 Sorry!, you've run out of attempts. The number was ${randomNumber}.`;
-            feedback.style.color = 'red';
-            valueButton.disabled = true;
-        }
-
-        valueInput.value = '';
+    // To check if input is not a number....
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+      feedback.textContent =
+        "😁 Kindly enter a valid number between 1 and 100.";
+      feedback.style.color = "red";
+      return;
     }
-    // Add click event listener to the guess button
-    valueButton.addEventListener('click', handleGuess);
 
-    // Add keypress event listener to the input field
-    valueInput.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-            handleGuess();
-        }
-    });
+    attempts--;
+
+    if (userGuess === randomNumber) {
+      feedback.textContent = `🎉 Congratulations! You guessed the right number (${randomNumber}).`;
+      feedback.style.color = "green";
+      valueButton.disabled = true;
+    } else if (attempts > 0) {
+      feedback.textContent =
+        userGuess < randomNumber ? "🙄 Too low!," : "🤨 Too high!,";
+      feedback.style.color = "purple";
+      feedback.textContent += ` You have ${attempts} ${
+        attempts === 1 ? "attempt" : "attempts"
+      } left.`;
+    } else {
+      feedback.textContent = `😔 Sorry!, you've run out of attempts. The number was ${randomNumber}.`;
+      feedback.style.color = "red";
+      valueButton.disabled = true;
+    }
+
+    valueInput.value = "";
+  }
+  // Add click event listener to the guess button
+  valueButton.addEventListener("click", handleGuess);
+
+  // Add keypress event listener to the input field
+  valueInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      handleGuess();
+    }
+  });
 });
-
